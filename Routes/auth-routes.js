278f5cd93add520d2953c148322772676a589c82
waskylinks <likitajoel@gmail.com>
2025-11-11 17:07:25 +0100

    feat: add auth path to change password logic

--- Routes/auth-routes.js
+++ Routes/auth-routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const router = express.Router();
 const {loginUser, registerUser, changePassword} = require('../Controllers/auth-controller');
+const authMiddleware = require('../Middleware/auth-middleware');
+
+const router = express.Router();
 
 //all routes related to auth
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/change-password', changePassword);
+router.post('/change-password', authMiddleware, changePassword);
 
 
 
